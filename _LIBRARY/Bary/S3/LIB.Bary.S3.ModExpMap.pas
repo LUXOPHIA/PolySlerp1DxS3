68222940fbe42a0c81eef0b703bc43f01fbb2006
luxidea <contact@luxidea.net>
2025-02-10 01:54:29 +0900

    LIB.Bary.S3.* ルーチンを LUX.S3.Curve ライブラリに移動。

--- _LIBRARY/Bary/S3/LIB.Bary.S3.ModExpMap.pas
+++ _LIBRARY/Bary/S3/LIB.Bary.S3.ModExpMap.pas
@@ -25,16 +25,9 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ModExpMap
-
-function ModExpMap( const Qs_:TArray<TSingleW3S> ) :TSingleW3S; overload;
-function ModExpMap( const Qs_:TArray<TDoubleW3S> ) :TDoubleW3S; overload;
-
 implementation //############################################################### ■
 
-uses LUX,
-     LUX.Quaternion,
-     LIB.Bary.S3.Glerp;
+uses LUX.S3.Curve.ModExpMap;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
@@ -52,57 +45,5 @@ begin
 end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ModExpMap
-
-function ModExpMap( const Qs_:TArray<TSingleW3S> ) :TSingleW3S;
-var
-   G, Gi :TSingle3S;
-   Q :TSingleW3S;
-begin
-     G := Glerp( Qs_ ).v;  Gi := G.Inv;
-
-     Result := TSingleW3S.Create( 0, 0 );
-
-     for Q in Qs_ do Result.w := Result.w + Q.w;
-
-     if Abs( Result.w ) < SINGLE_EPS3 then
-     begin
-          for Q in Qs_ do Result.v := Result.v + Ln( Gi * Q.v );
-
-          Result.v := G * Exp( Result.v );
-     end
-     else
-     begin
-          for Q in Qs_ do Result.v := Result.v + Q.w * Ln( Gi * Q.v );
-
-          Result.v := G * Exp( Result.v / Result.w );
-     end;
-end;
-
-function ModExpMap( const Qs_:TArray<TDoubleW3S> ) :TDoubleW3S;
-var
-   G, Gi :TDouble3S;
-   Q :TDoubleW3S;
-begin
-     G := Glerp( Qs_ ).v;  Gi := G.Inv;
-
-     Result := TDoubleW3S.Create( 0, 0 );
-
-     for Q in Qs_ do Result.w := Result.w + Q.w;
-
-     if Abs( Result.w ) < DOUBLE_EPS3 then
-     begin
-          for Q in Qs_ do Result.v := Result.v + Ln( Gi * Q.v );
-
-          Result.v := G * Exp( Result.v );
-     end
-     else
-     begin
-          for Q in Qs_ do Result.v := Result.v + Q.w * Ln( Gi * Q.v );
-
-          Result.v := G * Exp( Result.v / Result.w );
-     end;
-end;
 
 end. //######################################################################### ■
